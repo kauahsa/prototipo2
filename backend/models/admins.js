f5488dc4d5790d06ac52bc6ea80a2ad514ea544c
KauaAndrade-Unicamp <kauaestudosprog@gmail.com>
2025-07-15 08:46:26 -0300

    changes

--- backend/models/admins.js
+++
@@ -1,9 +0,0 @@
-const mongoose =  require('mongoose')
-
-const AdminSchema = new mongoose.Schema({
-  nome: String,
-  email: String,
-  password: String,
-  ativo: Boolean
-})
-module.exports = mongoose.model('Admin', AdminSchema, 'admins')
